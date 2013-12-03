30016e10666f4575e44ed05585842e8b1786580f
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-12-03 23:45:42 +0100

    rewrote with prototypes

--- index.js
+++ index.js
@@ -1,89 +1,147 @@
-var through = require('through2');
 var Promise = require('bluebird');
+var util = require('util');
+var Transform = require('stream').Transform;
 
+function nothing(x) { }
 function identity(x) { return x; }
 
-function defaults(opts) {
+function defaults(opts, fn, end) {
+    if (typeof(opts) === 'function') { 
+        end = fn; fn = opts; opts = {}; 
+    }
+    if (!fn) fn = identity;
     if (typeof(opts.objectMode) === 'undefined')
         opts.objectMode = true;
     if (opts.limit)
         opts.highWaterMark = opts.limit;
-    return opts;
+    return {opts: opts, fn: fn, end: end};
 }
 
-var exports = module.exports = function promiseThrough(opts, fn) {
-    if (!fn) { fn = opts; opts = {}; }
+// PromiseStream
 
-    var stream = through(defaults(opts), incoming, ended);
-
-    if (!fn) fn = identity;
-    fn = fn.bind(stream);
-
-    function incoming(data, enc, done) {     
-        Promise.cast([data, enc]).spread(fn)
-        .done(function(v) { done();  }, 
-              function(e) { done(e); });
-    }
-
-    function ended() { 
-        this.push(null);
-        streamEnd.fulfill(); 
-    }
-
-
-    var oldpush = stream.push.bind(stream);
-    var streamEnd = Promise.defer();
-
-    stream.promise = streamEnd.promise;
-
-    stream.push = function push(data) {       
-        return Promise.cast(data).then(oldpush); 
-    }
-
-    stream.map = function(mopts, f) {
-        if (!f) { f = mopts; mopts = opts; }
-        var mstream = exports.map(mopts, f);
-        stream.pipe(mstream);
-        return mstream;
-    }
-    stream.reduce = function(mopts, f, initial) {        
-        if (!f) { f = mopts; mopts = {}; }
-        defaults(mopts);
-        var reducer = exports.reduce(mopts, f, initial);
-        stream.pipe(reducer);
-        return reducer.promise;
-    }
-    return stream;
+util.inherits(PromiseStream, Transform);
+function PromiseStream(args) {
+    if (!(this instanceof PromiseStream))
+        return new PromiseStream(args)
+    this.args = args;
+    this._streamEnd = Promise.defer();
+    Transform.call(this, args.opts);
 }
 
-exports.map = function(mopts, f) {
-    if (!f) { f = mopts; mopts = {}; }
-    defaults(mopts);
-    var smap = exports(mopts, function(el) {
-        return this.push(f(el));
-    })
-    return smap;
+PromiseStream.prototype._transform = incoming;
+function incoming(data, enc, done) {     
+    Promise.cast([data, enc])
+    .bind(this)
+    .spread(this.args.fn)
+    .then(nothing) // to avoid passing values
+    .then(nothing)
+    .done(done, done);
+}
+
+PromiseStream.prototype._flush = complete
+function complete(done) {
+    if (!this.args.end) return done();
+    Promise.fulfilled()
+    .bind(this)
+    .then(this.args.end)
+    .then(nothing)
+    .done(done, done);
+
+}
+
+PromiseStream.prototype.push = push;
+function push(data) {
+    return Promise.cast(data)
+    .bind(this)
+    .then(Transform.prototype.push); 
+}
+
+PromiseStream.prototype.map = map;
+function map(opts, fn, end) {
+    var mstream = exports.map(opts, fn);
+    this.pipe(mstream);
+    return mstream;
+}
+
+PromiseStream.prototype.reduce = reduce;
+function reduce(opts, fn, initial) {
+    var reducer = exports.reduce(opts, fn, initial);
+    this.pipe(reducer);
+    return reducer.promise();
+}
+
+PromiseStream.prototype.promise = promise;
+function promise() {
+    return this._streamEnd;
+}
+
+// MapPromiseStream
+
+util.inherits(MapPromiseStream, PromiseStream);
+function MapPromiseStream(args) {
+    PromiseStream.call(this, args);
+    this.args.mapfn = args.fn;
+    this.args.fn = mapStreamFn;
+}
+
+function mapStreamFn(el) {
+    return this.push(this.args.mapfn(el));
+}    
+
+// ReducePromiseStream
+
+util.inherits(ReducePromiseStream, PromiseStream);
+function ReducePromiseStream(args) {
+    PromiseStream.call(this, args);
+    this.args.acc = null;
+    this.args.reducefn = args.fn;
+    this.args.fn = reduceStreamFn;
+    this.args.end = reduceStreamEnd;
+    this.promise = reduceStreamPromise;
+    this._defer = Promise.defer();
+    this.on('error', this.rejectPromise);
+}
+
+function reduceStreamPromise() {
+    return this._defer.promise;
+}
+
+ReducePromiseStream.prototype.rejectPromise = function(e) {
+    this._defer.reject(e);
+}
+
+function reduceStreamFn(el) {   
+    var initial = this.args.initial,
+        acc = this.args.acc;
+    if (acc === null) 
+        acc = initial ? Promise.cast(initial) 
+            : Promise.cast(el);
+    else            
+        acc = Promise.all([acc, el])
+            .spread(this.args.reducefn);
+    this.args.acc = acc;            
+    return this.push(acc);
+
+}
+
+function reduceStreamEnd() {
+    return this.args.acc
+        .bind(this._defer)
+        .then(this._defer.fulfill);
+}
+
+// API
+
+exports.through = function promiseThrough(opts, fn, end) {
+    var args = defaults(opts, fn, end);
+    return new PromiseStream(args);
+}
+
+exports.map = function promiseMap (mopts, f) {
+    return new MapPromiseStream(defaults(mopts, f));
 };
 
 exports.reduce = function(mopts, f, initial) {
-    if (!f) { f = mopts; mopts = {}; }
-    defaults(mopts);
-    var def = Promise.defer(), acc;
-
-    var x = exports(mopts, function(el) {
-        if (typeof(acc) === 'undefined') 
-            acc = initial ? Promise.cast(initial) 
-        : Promise.cast(el);
-        else            
-            acc = Promise.all([acc, el]).spread(f);
-        return this.push(acc);
-    });
-    x.promise.then(function ended() {
-        def.resolve(acc); 
-    });
-    x.on('error', function errored(e) {
-        def.reject(e) 
-    });
-    x.promise = def.promise;
-    return x;
+    return new ReducePromiseStream(defaults(mopts, f, initial));
 }
+
--- test/test.js
+++ test/test.js
@@ -1,25 +1,24 @@
 
-var pthrough = require('../');
+var ps = require('../');
 var B = require('bluebird');
 var fs = require('fs');
 var split = require('split');
 var path = require('path');
-var through = require('through2');
 
 fs.createReadStream(path.join(__dirname, 'test.txt'), 'utf8')
     .pipe(split())
-    .pipe(pthrough({tag: 'start'}, function(line) {
+    .pipe(ps.through(function(line) {
         console.log("Received", line, typeof(line));
         var delayed = B.delay(100).then(function() {
             return line ? parseFloat(line) : null;
         });
         return this.push(delayed);
     }))
-    .map({tag: 'map'}, function(el) {
+    .map(function(el) {
         console.log('Multiply', el, typeof(el));
         return el * 2;
     })
-    .reduce({tag: 'reduce'}, function(acc, el) {
+    .reduce(function(acc, el) {
         console.log("Accumulate", acc, el, typeof(el));
         return acc + el;
     })
