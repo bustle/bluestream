3c846fa9dfd010ea37a77e42eaacac59e57cb054
TSiege <tristan.t.siegel@gmail.com>
2018-02-25 12:27:46 -0500

    added type definitions

---
+++ types/index.d.ts
@@ -0,0 +1,78 @@
+// Type definitions for bluestream 6.1
+// Project: https://github.com/bustle/bluestream
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface DeferReturn {
+  resolve: Function,
+  reject: Function,
+  promise: Promise<any>
+}
+
+export class FilterStream {
+  constructor(options: object, fn: Function);
+}
+
+export class ReadStream {
+  constructor(options: object, fn: Function);
+
+  emitError(...args: any[]): void;
+
+  promise(): Promise<any>;
+
+  push(...args: any[]): void;
+
+  static ReadableState(options: any, stream: any): void;
+}
+
+export class ReduceStream {
+  constructor(options: object, fn: Function);
+
+  promise(): Promise<any>;
+}
+
+export class TransformStream {
+  constructor(options: object, fn: Function);
+
+  emitError(e: Error): void;
+
+  end(chunk: any, encoding: string, cb: Function): void;
+
+  promise(): Promise<any>;
+
+  push(...args: any[]): void;
+}
+
+export class WriteStream {
+  constructor(options: object, fn: Function);
+
+  emitError(...args: any[]): void;
+
+  end(chunk: any, encoding: string, cb: Function): void;
+
+  promise(): Promise<any>;
+
+  wait(...args: any[]): void;
+}
+
+export function collect(stream: ReadStream|WriteStream|TransformStream): any[];
+
+export function filter(opts: object, fn: Function): void;
+
+export function map(opts: object, fn: Function): void;
+
+export function pipe(...streams: (ReadStream|WriteStream|TransformStream)[]): any;
+
+export function read(opts: object, readFn: Function): void;
+
+export function reduce(opts: object, fn: Function, initial: any): void;
+
+export function transform(opts: object, fn: Function): void;
+
+export function wait(stream: any): Promise<ReadStream|WriteStream|TransformStream>;
+
+export function maybeResume(stream: ReadStream|WriteStream|TransformStream): Promise<ReadStream|WriteStream|TransformStream>;
+
+export function defer(): DeferReturn;
+
+export function write(opts: object, writeFn: Function): void;
